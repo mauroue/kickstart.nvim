1af87de293fed84d9c05c2b5e56a63ef802a5089
Mauro Tatsuya Ue <mauroue@gmail.com>
2024-04-24 08:13:04 -0300

    new keybind to delete buffer

--- init.lua
+++ init.lua
@@ -353,11 +353,12 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          path_display = 'truncate',
+          mappings = {
+            i = { ['<c-d>'] = 'delete_buffer' },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
